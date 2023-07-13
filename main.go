4eb24b67bebb856294389de729ec1c2f76edd31c
Lucky7Tb <luckytri101@gmail.com>
2023-07-13 16:51:08 +0700

    add base url env

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	docs.SwaggerInfo.Title = "Tulisaja api"
 	docs.SwaggerInfo.Description = "Api documentation for tulisaja app"
 	docs.SwaggerInfo.Version = "1.0"
-	docs.SwaggerInfo.Host = "localhost:3000"
+	docs.SwaggerInfo.Host = os.Getenv("BASE_URL")
 	docs.SwaggerInfo.Schemes = []string{"http", "https"}
 	docs.SwaggerInfo.BasePath = "/api"
 
